9f3a0c5564212346ecc35068de45f570a6b51bf8
mala <Martin@Michel-Lange.net>
2023-08-28 23:25:21 +0200

    Handle exceptions in Main, write out error readable and set exit code

--- AnyRest/Program.cs
+++ AnyRest/Program.cs
@@ -5,22 +5,36 @@ namespace AnyRest
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args.Length == 1 && args[0] == "-io")
             {
                 StreamUtils.StdInToStdOut();
+                return 0;
             }
             else if (args.Length == 2 && args[0] == "-s")
             {
                 int time;
                 if (Int32.TryParse(args[1], out time))
+                {
                     Thread.Sleep(time);
+                    return 0;
+                }
+                return 1;
             }
             else
             {
                 var filename = "config.json";
-                Endpoints endpoints = FileConfig.LoadFromFile(filename);
+                Endpoints endpoints;
+                try
+                {
+                    endpoints = FileConfig.LoadFromFile(filename);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Config error: {ex.Message}");
+                    return 1;
+                }
                 Console.WriteLine($"Endpoints from file {filename}");
                 foreach (var endpoint in endpoints)
                 {
@@ -29,6 +43,8 @@ namespace AnyRest
                 Console.WriteLine("");
                 Console.WriteLine("Starting service");
                 Service.StartService(endpoints);
+
+                return 0;
             }
         }
     }
